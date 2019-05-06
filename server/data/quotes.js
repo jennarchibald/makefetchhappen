0795524545fbc6ab184e34f4bbf9685934a924f3
jennarchibald <jennarchibald91@gmail.com>
2019-05-06 13:03:31 +0100

    css changes

--- server/data/quotes.js
+++ server/data/quotes.js
@@ -1,49 +1,143 @@
 module.exports = [
-   {"quote": "Get in loser. We’re going shopping."},
- {"quote": "Ex-boyfriends are off-limits to friends. That’s just, like, the rules of feminism."},
- {"quote": "Raise your hand if you have ever been personally victimized by Regina George."},
- {"quote": "We only carry sizes one, three and five. You could try Sears."},
- {"quote": "That’s why her hair is so big. It’s full of secrets."},
- {"quote": "It’s like I have ESPN or something. My breasts can always tell when it’s going to rain. Well… they can tell when it’s raining."},
- {"quote": "But you’re, like, really pretty… So you agree? You think you’re really pretty?"},
- {"quote": "I’m a mouse, duh."},
- {"quote": "Can I get you guys anything? Some snacks? A condom? Let me know! Oh, God love ya."},
- {"quote": "I don’t hate you ‘cause your fat. You’re fat ‘cause I hate you!"},
- {"quote": "I wish we could all get along like we used to in middle school. I wish I could bake a cake filled with rainbows and smiles and everyone would eat and be happy."},
- {"quote": "One time, she punched me in the face. It was awesome."},
- {"quote": "I want to lose three pounds."},
- {"quote": "’Cause she’s a life ruiner. She ruins people’s lives."},
- {"quote": "Regina George is not sweet! She’s a scum-sucking road whore, she ruined my life!"},
- {"quote": "If only you knew how mean she really is, you’d know that I’m not allowed to wear hoop earrings, right? Yeah, two years ago she told me hoops earrings were her thing, and I wasn’t allowed to wear them anymore. And then for Hanukkah my parents got this pair of really expensive white gold hoops and I had to pretend like I didn’t even like them. It was so sad."},
- {"quote": "On Wednesdays we wear pink."},
- {"quote": "Did you see nipple? It only counts if you saw a nipple!"},
- {"quote": "Why should Caesar get to stomp around like a giant, while the rest of us try not to get smushed under his big feet? What’s so great about Caesar? Hmm? Brutus is just as cute as Caesar. Brutus is just as smart as Caesar. People totally like Brutus just as much as they like Caesar. And when did it become okay for one person to be the boss of everybody, huh? Because that’s not what Rome is about. We should totally just stab Caesar!"},
- {"quote": "You smell like a baby prostitute."},
- {"quote": "I’m sorry I called you a gap-toothed bitch. It’s not your fault you’re so gap-toothed."},
- {"quote": "Everyone in Africa can read Swedish."},
- {"quote": "I can’t go to Taco Bell. I’m on an all-carb diet. God, Karen, you are so stupid!"},
- {"quote": "There are two kinds of evil people in this world. Those who do evil stuff and those who see evil stuff being done and don’t try to stop it."},
- {"quote": "I’m not a regular mom, I’m a cool mom."},
- {"quote": "Gretchen, I’m sorry I laughed at you that time you got diarrhea at Barnes & Nobles. And I’m sorry for telling everyone about it. And I’m sorry for repeating it now."},
- {"quote": "She doesn’t even go here!"},
- {"quote": "I hear she does car commercials…in Japan."},
- {"quote": "Whatever, I’m getting cheese fries."},
- {"quote": "I have this theory, that if you cut off all her hair she’d look like a British man."},
- {"quote": "Four for you, Glenn Coco! You go, Glenn Coco!"},
- {"quote": "How many of you have ever felt personally victimised by Regina George?"},
- {"quote": "It’s so fetch!"},
- {"quote": "The limit does not exist!"},
- {"quote": "I know it may look like I was being like a bitch, but that's only because I was acting like a bitch."},
- {"quote": "Boo! You Whore!"},
- {"quote": "You can't sit with us!"},
- {"quote": "My nanna takes off her wig when she's drunk..."},
- {"quote": "Is butter a carb?"},
- {"quote": "I'm sorry that people are so jealous of me. But I can't help it that I'm popular."},
- {"quote": "Don't have sex. Because you will get pregnant. And die."},
- {"quote": "Stop trying to make fetch happen! It's not going to happen!"},
- {"quote": "On October 3rd, he asked me what day it was."},
- {"quote": "I love her, she's like a martian!"},
- {"quote": "So that's against the rules, and you can't sit with us."},
- {"quote": "She asked me how to spell orange."},
- {"quote": "Oh, I love seeing teachers outside of school. It's like seeing a dog walk on its hind legs."}
+  {
+    "quote": "Get in loser. We’re going shopping."
+  },
+  {
+    "quote": "Ex-boyfriends are off-limits to friends. That’s just, like, the rules of feminism."
+  },
+  {
+    "quote": "Raise your hand if you have ever been personally victimized by Regina George."
+  },
+  {
+    "quote": "We only carry sizes one, three and five. You could try Sears."
+  },
+  {
+    "quote": "That’s why her hair is so big. It’s full of secrets."
+  },
+  {
+    "quote": "It’s like I have ESPN or something. My breasts can always tell when it’s going to rain. Well… they can tell when it’s raining."
+  },
+  {
+    "quote": "But you’re, like, really pretty… So you agree? You think you’re really pretty?"
+  },
+  {
+    "quote": "I’m a mouse, duh."
+  },
+  {
+    "quote": "Can I get you guys anything? Some snacks? A condom? Let me know! Oh, God love ya."
+  },
+  {
+    "quote": "I don’t hate you ‘cause your fat. You’re fat ‘cause I hate you!"
+  },
+  {
+    "quote": "I wish we could all get along like we used to in middle school. I wish I could bake a cake filled with rainbows and smiles and everyone would eat and be happy."
+  },
+  {
+    "quote": "One time, she punched me in the face. It was awesome."
+  },
+  {
+    "quote": "I want to lose three pounds."
+  },
+  {
+    "quote": "’Cause she’s a life ruiner. She ruins people’s lives."
+  },
+  {
+    "quote": "Regina George is not sweet! She’s a scum-sucking road whore, she ruined my life!"
+  },
+  {
+    "quote": "If only you knew how mean she really is, you’d know that I’m not allowed to wear hoop earrings, right? Yeah, two years ago she told me hoops earrings were her thing, and I wasn’t allowed to wear them anymore. And then for Hanukkah my parents got this pair of really expensive white gold hoops and I had to pretend like I didn’t even like them. It was so sad."
+  },
+  {
+    "quote": "On Wednesdays we wear pink."
+  },
+  {
+    "quote": "Did you see nipple? It only counts if you saw a nipple!"
+  },
+  {
+    "quote": "Why should Caesar get to stomp around like a giant, while the rest of us try not to get smushed under his big feet? What’s so great about Caesar? Hmm? Brutus is just as cute as Caesar. Brutus is just as smart as Caesar. People totally like Brutus just as much as they like Caesar. And when did it become okay for one person to be the boss of everybody, huh? Because that’s not what Rome is about. We should totally just stab Caesar!"
+  },
+  {
+    "quote": "You smell like a baby prostitute."
+  },
+  {
+    "quote": "I’m sorry I called you a gap-toothed bitch. It’s not your fault you’re so gap-toothed."
+  },
+  {
+    "quote": "Everyone in Africa can read Swedish."
+  },
+  {
+    "quote": "I can’t go to Taco Bell. I’m on an all-carb diet. God, Karen, you are so stupid!"
+  },
+  {
+    "quote": "There are two kinds of evil people in this world. Those who do evil stuff and those who see evil stuff being done and don’t try to stop it."
+  },
+  {
+    "quote": "I’m not a regular mom, I’m a cool mom."
+  },
+  {
+    "quote": "Gretchen, I’m sorry I laughed at you that time you got diarrhea at Barnes & Nobles. And I’m sorry for telling everyone about it. And I’m sorry for repeating it now."
+  },
+  {
+    "quote": "She doesn’t even go here!"
+  },
+  {
+    "quote": "I hear she does car commercials…in Japan."
+  },
+  {
+    "quote": "Whatever, I’m getting cheese fries."
+  },
+  {
+    "quote": "I have this theory, that if you cut off all her hair she’d look like a British man."
+  },
+  {
+    "quote": "Four for you, Glenn Coco! You go, Glenn Coco!"
+  },
+  {
+    "quote": "How many of you have ever felt personally victimised by Regina George?"
+  },
+  {
+    "quote": "It’s so fetch!"
+  },
+  {
+    "quote": "The limit does not exist!"
+  },
+  {
+    "quote": "I know it may look like I was being like a bitch, but that's only because I was acting like a bitch."
+  },
+  {
+    "quote": "Boo! You Whore!"
+  },
+  {
+    "quote": "You can't sit with us!"
+  },
+  {
+    "quote": "My nanna takes off her wig when she's drunk..."
+  },
+  {
+    "quote": "Is butter a carb?"
+  },
+  {
+    "quote": "I'm sorry that people are so jealous of me. But I can't help it that I'm popular."
+  },
+  {
+    "quote": "Don't have sex. Because you will get pregnant. And die."
+  },
+  {
+    "quote": "Stop trying to make fetch happen! It's not going to happen!"
+  },
+  {
+    "quote": "On October 3rd, he asked me what day it was."
+  },
+  {
+    "quote": "I love her, she's like a martian!"
+  },
+  {
+    "quote": "So that's against the rules, and you can't sit with us."
+  },
+  {
+    "quote": "She asked me how to spell orange."
+  },
+  {
+    "quote": "Oh, I love seeing teachers outside of school. It's like seeing a dog walk on its hind legs."
+  }
 ];
